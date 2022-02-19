0018651666613901a5ed90ef38d9c92f61c7bb50
allexib <ooomegasib@gmail.com>
2022-02-19 16:39:12 +0300

    update user data

--- mediumclone-nestjs/src/user/user.controller.ts
+++ mediumclone-nestjs/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
@@ -6,6 +6,7 @@ import { LoginUserDto } from '@app/user/dto/login.dto';
 import { User } from '@app/user/decarators/user.decorator';
 import { UserEntity } from '@app/user/user.entity';
 import { AuthGuard } from '@app/user/guards/auth.guard';
+import { UpdateUserDto } from '@app/user/dto/updateUser.dto';
 
 @Controller()
 export class UserController {
@@ -33,9 +34,19 @@ export class UserController {
   @UseGuards(AuthGuard)
   async currentUser(
     @User()user: UserEntity,
-   /* @User('id') currentUserId: number,*/
+    /* @User('id') currentUserId: number,*/
   ): Promise<UserResponseInterface> {
     /*console.log('userId', currentUserId);*/
     return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateCurrentUser(
+    @User('id')currentUserId: number,
+    @Body('user') updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseInterface> {
+  const user=await this.userService.updateUser(currentUserId,updateUserDto)
+    return this.userService.buildUserResponse(user)
   }
 }
--- mediumclone-nestjs/src/user/user.service.ts
+++ mediumclone-nestjs/src/user/user.service.ts
@@ -8,6 +8,7 @@ import { JWT_SECRET } from '@app/config';
 import { UserResponseInterface } from '@app/user/types/userResponse.interface';
 import { LoginUserDto } from '@app/user/dto/login.dto';
 import { compare } from 'bcrypt';
+import { UpdateUserDto } from '@app/user/dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
@@ -62,8 +63,14 @@ export class UserService {
     return user;
   }
 
-   findById(id: number): Promise<UserEntity> {
+  findById(id: number): Promise<UserEntity> {
     return this.userRepository.findOne(id);
+  }
+
+  async updateUser(userId: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    const user = await this.findById(userId);
+    Object.assign(user, updateUserDto);
+    return await this.userRepository.save(user);
   }
 
   generateJwt(user: UserEntity): string {
